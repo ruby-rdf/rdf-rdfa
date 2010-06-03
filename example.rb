6d9e2b1aba1e49dc5d8d2a26bb3c6916e92c1a34
Nicholas J Humfrey <njh@aelius.com>
2010-06-03 12:15:09 +0100

    Fixed the example.rb.

--- example.rb
+++ example.rb
@@ -3,25 +3,24 @@
 $:.unshift(File.join(File.dirname(__FILE__), 'lib'))
 
 require 'rubygems'
-require 'rdfa/reader'
+require 'rdf/rdfa'
 
 data = <<-EOF;
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN" "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml"
-      xmlns:dc="http://purl.org/dc/elements/1.1/">
-<head>
-  <title>Test 0001</title>
-</head>
-<body>
-  <p>This photo was taken by <span class="author" about="photo1.jpg" property="dc:creator">Mark Birbeck</span>.</p>
-</body>
-</html>
+  <?xml version="1.0" encoding="UTF-8"?>
+  <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN" "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd"> 
+  <html xmlns="http://www.w3.org/1999/xhtml" version="XHTML+RDFa 1.0" 
+     xmlns:dc="http://purl.org/dc/elements/1.1/">
+  <head>
+     <title>Test 0001</title>
+  </head>
+  <body>
+     <p>This photo was taken by <span class="author" about="photo1.jpg" property="dc:creator">Mark Birbeck</span>.</p>
+  </body>
+  </html>
 EOF
 
-$DEBUG = false
-
-reader = RDFa::Reader.new(data, :debug => false)
-reader.each_statement do |statement|
-  statement.inspect!
+RDF::RDFa::Reader.new(data, :base_uri => 'http://rdfa.digitalbazaar.com/test-suite/test-cases/xhtml1/0001.xhtml') do |reader|
+  reader.each_statement do |statement|
+    statement.inspect!
+  end
 end
