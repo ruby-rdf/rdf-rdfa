a80efa71c79d3df9f9c7fa2f9e61305e6ffc376a
Nicholas J Humfrey <njh@aelius.com>
2010-06-03 11:54:17 +0100

    Split up specifications into one 'should' per 'it should' block.

--- spec/rdfa_reader_spec.rb
+++ spec/rdfa_reader_spec.rb
@@ -29,145 +29,220 @@ describe "RDF::RDFa::Reader" do
     readers.each { |reader| reader.should == RDF::RDFa::Reader }
   end
 
-   it "should parse simple doc" do
-    sampledoc = <<-EOF;
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN" "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
-    <html xmlns="http://www.w3.org/1999/xhtml"
-          xmlns:dc="http://purl.org/dc/elements/1.1/">
-    <head>
-    	<title>Test 0001</title>
-    </head>
-    <body>
-    	<p>This photo was taken by <span class="author" about="photo1.jpg" property="dc:creator">Mark Birbeck</span>.</p>
-    </body>
-    </html>
-    EOF
-
-    reader = RDF::RDFa::Reader.new(sampledoc, :base_uri => "http://rdfa.digitalbazaar.com/test-suite/test-cases/xhtml1/0001.xhtml", :strict => true)
-    reader.graph.size.should == 1
-    reader.graph.should have_object("Mark Birbeck")
-  end
-
-  it "should parse simple doc without a base URI" do
-    sampledoc = <<-EOF;
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN" "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
-    <html xmlns="http://www.w3.org/1999/xhtml"
-          xmlns:dc="http://purl.org/dc/elements/1.1/">
-    <body>
-    	<p>This photo was taken by <span class="author" about="_:photo" property="dc:creator">Mark Birbeck</span>.</p>
-    </body>
-    </html>
-    EOF
-
-    reader = RDF::RDFa::Reader.new(sampledoc, :strict => true)
-    reader.graph.size.should == 1
-    reader.graph.should have_triple([RDF::Node('photo'), RDF::DC11.creator, "Mark Birbeck"])
-  end
-
-  it "should parse an XML Literal" do
-    sampledoc = <<-EOF
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN" "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
-    <html xmlns="http://www.w3.org/1999/xhtml"
-          xmlns:dc="http://purl.org/dc/elements/1.1/">
-    	<head>
-    		<title>Test 0011</title>
-    	</head>
-      <body>
-      	<div about="">
-          Author: <span property="dc:creator">Albert Einstein</span>
-          <h2 property="dc:title">E = mc<sup>2</sup>: The Most Urgent Problem of Our Time</h2>
-    	</div>
-      </body>
-    </html>
-    EOF
-
-    reader = RDF::RDFa::Reader.new(sampledoc, :base_uri => "http://rdfa.digitalbazaar.com/test-suite/test-cases/xhtml1/0011.xhtml", :strict => true)
-    reader.graph.size.should == 2
-
-    reader.graph.should have_object("Albert Einstein")
-    reader.graph.should have_object(
-      RDF::Literal("E = mc<sup>2</sup>: The Most Urgent Problem of Our Time",
-        :datatype => RDF.XMLLiteral)
-    )
-  end
-
-
-  it "should parse BNodes" do
-    sampledoc = <<-EOF
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN" "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
-    <html xmlns="http://www.w3.org/1999/xhtml"
-          xmlns:foaf="http://xmlns.com/foaf/0.1/">
+  context "paring a simple doc" do
+    before :each do
+      sampledoc = <<-EOF;
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN" "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
+      <html xmlns="http://www.w3.org/1999/xhtml"
+            xmlns:dc="http://purl.org/dc/elements/1.1/">
       <head>
-    	<title>Test 0017</title>   
+        <title>Test 0001</title>
       </head>
       <body>
-      	 <p>
-            <span about="[_:a]" property="foaf:name">Manu Sporny</span>
-            <span about="[_:a]" rel="foaf:knows" resource="[_:b]">knows</span>
-            <span about="[_:b]" property="foaf:name">Ralph Swick</span>.
-         </p>
+        <p>This photo was taken by <span class="author" about="photo1.jpg" property="dc:creator">Mark Birbeck</span>.</p>
       </body>
-    </html>
-    EOF
-
-    reader = RDF::RDFa::Reader.new(sampledoc, :base_uri => "http://rdfa.digitalbazaar.com/test-suite/test-cases/xhtml1/0011.xhtml", :strict => true)
-
-    reader.graph.size.should == 3
-    reader.graph.should have_object("Ralph Swick")
-    reader.graph.should have_object("Manu Sporny")
-  end
+      </html>
+      EOF
   
-   def self.test_cases(suite)
-     [] #RdfaHelper::TestCase.test_cases(suite)
-   end
-
-   # W3C Test suite from http://www.w3.org/2006/07/SWD/RDFa/testsuite/
-   %w(xhtml html4 html5).each do |suite|
-     describe "w3c #{suite} testcases" do
-       describe "that are approved" do
-         test_cases(suite).each do |t|
-           puts t.inspect
-           next unless t.status == "approved"
-           #next unless t.name =~ /0140/
-           specify "test #{t.name}: #{t.title}#{",  (negative test)" unless t.expectedResults}" do
-             #puts t.input
-             #puts t.results
-             begin
-               t.run_test do |rdfa_string, rdfa_parser|
-                 rdfa_parser.parse(rdfa_string, t.informationResourceInput, :debug => [])
-               end
-             rescue SparqlException => e
-               pending(e.message) { raise }
-             end
-           end
-         end
-       end
-       describe "that are unreviewed" do
-         test_cases(suite).each do |t|
-           next unless t.status == "unreviewed"
-           #next unless t.name =~ /0185/
-           #puts t.inspect
-           specify "test #{t.name}: #{t.title}#{",  (negative test)" unless t.expectedResults}" do
-             begin
-               t.run_test do |rdfa_string, rdfa_parser|
-                 rdfa_parser.parse(rdfa_string, t.informationResourceInput, :debug => [])
-               end
-             rescue SparqlException => e
-               pending(e.message) { raise }
-             rescue Spec::Expectations::ExpectationNotMetError => e
-               if t.name =~ /01[789]\d/
-                 raise
-               else
-                 pending() {  raise }
-               end
-             end
-           end
-         end
-       end
+      @reader = RDF::RDFa::Reader.new(sampledoc, :base_uri => "http://rdfa.digitalbazaar.com/test-suite/test-cases/xhtml1/0001.xhtml", :strict => true)
+      @statement = @reader.graph.statements.first
+    end
+    
+    it "should return 1 triple" do
+      @reader.graph.size.should == 1
+    end
+    
+    it "should have a subject with an expanded URI" do
+      @statement.subject.should == RDF::URI('http://rdfa.digitalbazaar.com/test-suite/test-cases/xhtml1/photo1.jpg')
+    end
+    
+    it "should have a predicate of dc:creator" do
+      @statement.predicate.should == RDF::DC11.creator
+    end
+    
+    it "should have an object of type literal and value 'Mark Birkbeck'" do
+      @statement.object.should == RDF::Literal("Mark Birbeck")
     end
   end
+
+  context "parsing a simple doc without a base URI" do
+    before :each do
+      sampledoc = <<-EOF;
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN" "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
+      <html xmlns="http://www.w3.org/1999/xhtml"
+            xmlns:dc="http://purl.org/dc/elements/1.1/">
+      <body>
+        <p>This photo was taken by <span class="author" about="_:photo" property="dc:creator">Mark Birbeck</span>.</p>
+      </body>
+      </html>
+      EOF
+
+      @reader = RDF::RDFa::Reader.new(sampledoc, :strict => true)
+      @statement = @reader.graph.statements.first
+    end
+    
+    it "should return 1 triple" do
+      @reader.graph.size.should == 1
+    end
+ 
+    it "should have a Blank Node named 'photo' as the subject of the triple" do
+      @statement.subject.should == RDF::Node('photo')
+    end
+    
+    it "should have a predicate of dc:creator" do
+      @statement.predicate.should == RDF::DC11.creator
+    end
+    
+    it "should have an object of type literal and value 'Mark Birkbeck'" do
+      @statement.object.should == RDF::Literal("Mark Birbeck")
+    end
+  end
+
+  context "parsing a document containing an XML Literal" do
+    before :each do
+      sampledoc = <<-EOF
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN" "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
+      <html xmlns="http://www.w3.org/1999/xhtml"
+            xmlns:dc="http://purl.org/dc/elements/1.1/">
+        <head>
+          <title>Test 0011</title>
+        </head>
+        <body>
+          <div about="">
+            Author: <span property="dc:creator">Albert Einstein</span>
+            <h2 property="dc:title">E = mc<sup>2</sup>: The Most Urgent Problem of Our Time</h2>
+        </div>
+        </body>
+      </html>
+      EOF
+  
+      @reader = RDF::RDFa::Reader.new(sampledoc, :base_uri => "http://rdfa.digitalbazaar.com/test-suite/test-cases/xhtml1/0011.xhtml", :strict => true)
+    end
+
+    it "should return 2 triples" do
+      @reader.graph.size.should == 2
+    end
+    
+    it "should return a triple for the dc:creator of the document" do
+      @reader.graph.should have_triple([
+        RDF::URI('http://rdfa.digitalbazaar.com/test-suite/test-cases/xhtml1/0011.xhtml'),
+        RDF::DC11.creator,
+        "Albert Einstein"
+      ])
+    end
+    
+    it "should return an XML Literal for the dc:title of the document" do
+      @reader.graph.should have_triple([
+        RDF::URI('http://rdfa.digitalbazaar.com/test-suite/test-cases/xhtml1/0011.xhtml'),
+        RDF::DC11.title,
+        RDF::Literal("E = mc<sup>2</sup>: The Most Urgent Problem of Our Time", :datatype => RDF.XMLLiteral)
+      ])
+    end
+  end
+
+  context "parsing a document containing sereral bnodes" do
+    before :each do
+      sampledoc = <<-EOF
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN" "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd"> 
+      <html xmlns="http://www.w3.org/1999/xhtml" version="XHTML+RDFa 1.0" 
+            xmlns:foaf="http://xmlns.com/foaf/0.1/">
+        <head>
+        <title>Test 0017</title>   
+        </head>
+        <body>
+           <p>
+                <span about="[_:a]" property="foaf:name">Manu Sporny</span>
+                 <span about="[_:a]" rel="foaf:knows" resource="[_:b]">knows</span>
+                 <span about="[_:b]" property="foaf:name">Ralph Swick</span>.
+              </p>
+        </body>
+      </html>
+      EOF
+      
+      @reader = RDF::RDFa::Reader.new(sampledoc, :base_uri => "http://rdfa.digitalbazaar.com/test-suite/test-cases/xhtml1/0017.xhtml", :strict => true)
+    end
+
+    it "should return 3 triples" do
+      @reader.graph.size.should == 3
+    end
+
+    it "should return a triple for the foaf:name of BNode A" do
+      @reader.graph.should have_triple([
+        RDF::Node('a'),
+        RDF::FOAF.name,
+        "Manu Sporny"
+      ])
+    end
+
+    it "should return a triple for the foaf:name of BNode B" do
+      @reader.graph.should have_triple([
+        RDF::Node('b'),
+        RDF::FOAF.name,
+        "Ralph Swick"
+      ])
+    end
+
+    it "should return a triple for BNode A knows BNode B" do
+      @reader.graph.should have_triple([
+        RDF::Node('a'),
+        RDF::FOAF.knows,
+        RDF::Node('b'),
+      ])
+    end
+  end
+
+  
+  def self.test_cases(suite)
+     [] #RdfaHelper::TestCase.test_cases(suite)
+  end
+
+  # W3C Test suite from http://www.w3.org/2006/07/SWD/RDFa/testsuite/
+  %w(xhtml html4 html5).each do |suite|
+    describe "w3c #{suite} testcases" do
+      describe "that are approved" do
+        test_cases(suite).each do |t|
+          puts t.inspect
+          next unless t.status == "approved"
+          #next unless t.name =~ /0140/
+          specify "test #{t.name}: #{t.title}#{",  (negative test)" unless t.expectedResults}" do
+            #puts t.input
+            #puts t.results
+            begin
+              t.run_test do |rdfa_string, rdfa_parser|
+                rdfa_parser.parse(rdfa_string, t.informationResourceInput, :debug => [])
+              end
+            rescue SparqlException => e
+              pending(e.message) { raise }
+            end
+          end
+        end
+      end
+      describe "that are unreviewed" do
+        test_cases(suite).each do |t|
+          next unless t.status == "unreviewed"
+          #next unless t.name =~ /0185/
+          #puts t.inspect
+          specify "test #{t.name}: #{t.title}#{",  (negative test)" unless t.expectedResults}" do
+            begin
+              t.run_test do |rdfa_string, rdfa_parser|
+                rdfa_parser.parse(rdfa_string, t.informationResourceInput, :debug => [])
+              end
+            rescue SparqlException => e
+              pending(e.message) { raise }
+            rescue Spec::Expectations::ExpectationNotMetError => e
+              if t.name =~ /01[789]\d/
+                raise
+              else
+                pending() {  raise }
+              end
+            end
+          end
+        end
+      end
+   end
+ end
 end
