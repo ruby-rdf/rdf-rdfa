25fc5cb8963378964560746385f70621e17cf8cd
Gregg Kellogg <gregg@kellogg-assoc.com>
2011-03-06 14:09:08 -0800

    Fix spec/rdfa_helper.rb stub_file.

--- spec/rdfa_helper.rb
+++ spec/rdfa_helper.rb
@@ -230,16 +230,16 @@ module RDF
 
     def self.stub_file(filename)
       case filename.to_s
-      when %r(http://rdfa.digitalbazaar.com/test-suite/profile/\w+)
-        filename.to_s.sub(%r(http://rdfa.digitalbazaar.com/test-suite/profile/\w+),
-          File.join(File.expand_path(File.dirname(__FILE__)), 'rdfa-test-suite', 'profile'))
-      when %r(http://rdfa.digitalbazaar.com/test-suite/test-cases/\w+)
-        filename.to_s.sub(%r(http://rdfa.digitalbazaar.com/test-suite/test-cases/\w+),
+      when %r(http://rdfa.digitalbazaar.com/test-suite/profiles/[\w\.]+)
+        filename.to_s.sub(%r(http://rdfa.digitalbazaar.com/test-suite/profiles),
+          File.join(File.expand_path(File.dirname(__FILE__)), 'rdfa-test-suite', 'profiles'))
+      when %r(http://rdfa.digitalbazaar.com/test-suite/test-cases/[\w\.]+)
+        filename.to_s.sub(%r(http://rdfa.digitalbazaar.com/test-suite/test-cases),
           File.join(File.expand_path(File.dirname(__FILE__)), 'rdfa-test-suite', 'tests'))
       when "http://www.w3.org/1999/xhtml/vocab"
         File.join(File.expand_path(File.dirname(__FILE__)), 'rdfa-test-suite', 'profile', "xhv")
       when "http://www.w3.org/2005/10/profile"
-        File.join(File.expand_path(File.dirname(__FILE__)), 'rdfa-test-suite', 'profile', "xhv")
+        File.join(File.expand_path(File.dirname(__FILE__)), '..', 'etc', "profile.html")
       when "http://example.com/profile"
         File.join(TMP_DIR, "profile.html")
       else
