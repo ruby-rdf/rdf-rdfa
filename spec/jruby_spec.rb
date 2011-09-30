10b83a55a03b71c3bebad0f7c7e81e05d6a08d53
Gregg Kellogg <gregg@kellogg-assoc.com>
2011-09-30 12:20:34 -0700

    Collecting some failing tests under Nokogiri for jRuby.

---
+++ spec/jruby_spec.rb
@@ -0,0 +1,24 @@
+# coding: utf-8
+$:.unshift "."
+require File.join(File.dirname(__FILE__), 'spec_helper')
+require 'rdf/spec/reader'
+
+# Some specific issues that fail with jRuby to be resolved
+describe Nokogiri::XML do
+  describe "parse" do
+    it "parses namespaced elements without a namespace" do
+      Nokogiri::XML.parse("<dc:sup>bar</dc:sup>").root.should_not be_nil
+    end
+  end
+end
+
+describe Nokogiri::HTML do
+  describe "xmlns" do
+    it "shows namespace definitions" do
+      doc = Nokogiri::HTML.parse(%q(<html xmlns:dc="http://purl.org/dc/elements/1.1/"></html>))
+      doc.root.namespace_definitions.should be_empty
+      doc.root.attributes.should_not be_empty
+    end
+  end
+end
+
