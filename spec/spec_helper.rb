5667b5dabfb35f372291d4b69069b41db3e4ecb9
Gregg Kellogg <gregg@greggkellogg.net>
2017-06-23 11:55:29 -0700

    Use test-suite from new rdfa.info using symlink for local access.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,6 @@ require "bundler/setup"
 require 'rubygems'
 require 'rspec'
 require 'yaml'
-require 'open-uri/cached'
 require 'rdf/isomorphic'
 require 'rdf/spec'
 require 'rdf/spec/matchers'
@@ -32,11 +31,6 @@ rescue LoadError
 end
 require 'rdf/rdfa'
 
-# Create and maintain a cache of downloaded URIs
-URI_CACHE = File.expand_path(File.join(File.dirname(__FILE__), "uri-cache"))
-Dir.mkdir(URI_CACHE) unless File.directory?(URI_CACHE)
-OpenURI::Cache.class_eval { @cache_path = URI_CACHE }
-
 ::RSpec.configure do |c|
   c.filter_run focus: true
   c.run_all_when_everything_filtered = true
@@ -44,14 +38,6 @@ OpenURI::Cache.class_eval { @cache_path = URI_CACHE }
     ruby:     lambda { |version| !(RUBY_VERSION.to_s =~ /^#{version}/) },
   }
   c.include(RDF::Spec::Matchers)
-end
-
-# For testing, modify RDF::Util::File.open_file to use Kernel.open, so we can just use open-uri-cached
-module RDF::Util::File
-  def self.open_file(filename_or_url, options = {}, &block)
-    options = options[:headers] || {} if filename_or_url.start_with?('http')
-    Kernel.open(filename_or_url, options, &block)
-  end
 end
 
 TMP_DIR = File.join(File.expand_path(File.dirname(__FILE__)), "tmp")
